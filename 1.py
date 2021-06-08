3cdbd9a5ee4b45b45b855120dcb369905250c307
QBx <ortynskiy-k.ros@mail.ru>
2021-06-08 23:19:12 +0300

    Выполненно 1 задание

---
+++ 1.py
@@ -0,0 +1,19 @@
+class TrafficLight():
+    __color = [['красный', 7], ['желтый', 2], ['зеленый', 5]]
+
+    def running(self):
+        import time
+        i = 0
+        print(self.__color[i][0])
+        while True:
+            local_time = self.__color[i][1]
+            if i == 2:
+                i = 0
+            else:
+                i += 1
+            time.sleep(local_time)
+            print(self.__color[i][0])
+
+
+a = TrafficLight()
+a.running()
